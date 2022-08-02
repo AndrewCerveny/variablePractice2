293de09256d94b2d041f5606e3d8bc60947416c9
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-02 15:30:34 -0400

    Add numbers type list

--- variables.js
+++ variables.js
@@ -4,3 +4,10 @@
 1. var name = "Andy";
 1. var day = "Tuesday";
 1. var favColor = "green";
+// anything  that assigns content between "" or ''
+
+## Numbers
+var monthsInYear = 12;
+var shoeSize = 13;
+var age = 28;
+// any number including decimals 
